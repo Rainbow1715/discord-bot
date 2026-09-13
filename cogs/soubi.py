2e7be7138ad05b2b81073d80ed2ea8b86f012c1a
Rainbow1715 <yama.nabana753@gmail.com>
2026-09-13 12:45:50 +0900

    Update soubi.py

--- cogs/soubi.py
+++ cogs/soubi.py
@@ -21,7 +21,79 @@ EQUIPMENT_MASTER = {
         "atk": 60,
         "description": "伝説の聖剣。"
     },
+
+        # 装備！！！！！
+        # EQUIPMENT_GACHA_POOL = {
+        #     "name": "鉄の剣", "rarity": 3, "icon": "🗡️", "atk_bonus": 15, "hp_bonus": 0, "desc": "一般的な鉄製の剣。"},
+
+    "なんか強そうな棒": {
+        "icon": "",
+        "rarity": 3,
+        "atk": 10,
+        "description": "その辺に落ちてそうな棒。強いのか？"
+    },
+    "紙パックのいちごオレ": {
+        "icon": "",
+        "rarity": 4,
+        "p_hp": 100,
+        "description": "美味しい。毎ターンHP100回復。"
+    },
+    "ナイフ": {
+        "icon": "",
+        "rarity": 3,
+        "atk": 10,
+        "description": "普通のナイフ。"
+    },
+    "ロケット": {
+        "icon": "",
+        "rarity": 3,
+        "atk": 10,
+        "description": "中に女性の写真が入っている。"
+    },
+    "電子機器": {
+        "icon": "",
+        "rarity": 5,
+        "atk": 500,
+        "description": "ばか"
+    },
+    "子供用カメラ": {
+        "icon": "",
+        "rarity": 3,
+        "atk": 10,
+        "description": "一応撮れる。"
+    },
+    "ハリセン": {
+        "icon": "",
+        "rarity": 3,
+        "atk": 10,
+        "description": "いい音が鳴りそう。"
+    },
+    "学校の箒": {
+        "icon": "",
+        "rarity": 3,
+        "atk": 10,
+        "description": "掃除でもするんですか？"
+    },
+    "鏡": {
+        "icon": "",
+        "rarity": 3,
+        "atk": 10,
+        "description": "この世で1番美しいのはだぁれ？"
+    },
+    "怪しい試験管": {
+        "icon": "",
+        "rarity": 3,
+        "p_hp": 50,
+        "description": "何が入ってるんですかこれ"
+    },
+    "酒瓶": {
+        "icon": "",
+        "rarity": 3,
+        "atk": 10,
+        "description": "割れると痛いですよ。"
+    },
 }
+
 
 class SoubiCog(commands.Cog):
     def __init__(self, bot):
