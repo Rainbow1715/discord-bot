1b9f77d39d5c52327d66be1f8c0be78b66bd26fd
Rainbow1715 <yama.nabana753@gmail.com>
2026-09-14 16:14:08 +0900

    Update feed.py

--- cogs/feed.py
+++ cogs/feed.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import database as db
-import achievements
+import achievement
 
 EAT_ACHIEVEMENT_MAP = {
     "竹村しえら": "eat_siera",
